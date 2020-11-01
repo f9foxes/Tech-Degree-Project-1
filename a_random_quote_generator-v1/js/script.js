4cdf2496485083af072d3120a8a507189a687d6c
Ben Fox <bfox11677@gmail.com>
2020-10-31 19:04:19 -0500

    finalized project

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,6 +10,8 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+
+// Array of quote objects to source random quotes from.
 const quotes = [
   {
     quote: 'On matters of style, swim with the current, on matters of principle, stand like a rock.',
@@ -27,7 +29,8 @@ const quotes = [
     quote: 'Try and fail, but don’t fail to try.',
     source: 'John Quincy Adams',
     citation: '',
-    year: ''
+    year: '',
+    favorite: 'Favorite Quote'
   },
   {
     quote: 'One man with courage makes a majority.',
@@ -37,7 +40,7 @@ const quotes = [
   },
   {
     quote: 'It is not strange… to mistake change for progress',
-    source: 'Millard Fillmore ',
+    source: 'Millard Fillmore',
     citation: '',
     year: ''
   },
@@ -49,24 +52,28 @@ const quotes = [
   },
   {
     quote: 'If wrinkles must be written on our brow, let them not be written on our heart. The spirit should never grow old.',
-    source: 'James Garfield ',
+    source: 'James Garfield',
     citation: '',
     year: ''
   },
 ]
 
-
+const colors = ['#a9cbcb', '#cbcba9', '#88976d', '#377369', '#0284d0', '#28a745', '#40e0d0',];
 
 /***
  * `getRandomQuote` function
 ***/
+
+// Function that accepts an array and returns a random object from that array (quotes).
 function getRandomQuote (arr) {
   let randomNum = Math.floor( Math.random() * arr.length) + 1;
   let quote = arr[randomNum -1];
+
   // console.log(randomNum);
   //console.log(quote);
   return quote;
 }
+
 //getRandomQuote(quotes);
 
 /***
@@ -74,24 +81,50 @@ function getRandomQuote (arr) {
 ***/
 
 function printQuote () {
-  let quoteToPrint = getRandomQuote(quotes);
-  console.log(quoteToPrint);
+  // Stores random quote object from quotes array to variable named quoteObject.
+  let quoteObject = getRandomQuote(quotes);
+  //console.log(quoteObject);
 
+  //String of HTML that will placed into the index.html  page.
   let html = `
-  <p class="quote"> ${quoteToPrint.quote} </p> 
-  <p class="source"> ${quoteToPrint.source}
+  <p class="quote"> ${quoteObject.quote} </p> 
+  <p class="source"> ${quoteObject.source}
   `;
-    
-    if (quoteToPrint.citation) {
-      html += `<span class="citation"> ${quoteToPrint.citation} </span>`
+
+    //Checks for a citation value in the random object: quoteObject. 
+    //If true then the  html string is added with the citation value.
+    if (quoteObject.citation) {
+      html += `<span class="citation"> ${quoteObject.citation} </span>`
     }
-    if (quoteToPrint.year) {
-      html += `<span class="year">${quoteToPrint.year}</span>`
+
+    //Checks for a name value in the random object: quoteObject.
+    //If true then the html string is added with the year value.
+    if (quoteObject.year) {
+      html += `<span class="year">${quoteObject.year}</span>`
     }
+
+    // Checks for favorite value in the random object: quoteObject.
+    // If true then the favorite value prints to the h1 element.
+    if (quoteObject.favorite) {
+      document.getElementById('favorite').innerHTML = quoteObject.favorite;
+    } else {
+      document.getElementById('favorite').innerHTML = `Random Quotes`;
+    }
+
+    //Closing  'p' tag added to close the second paragragh in the html string.
     html += '</p>'
-    console.log(html);
+    //console.log(html);
     
-    return document.getElementById('quote-box').innerHTML = html; 
+    // printQuote function prints the html string to the page. 
+    document.getElementById('quote-box').innerHTML = html; 
+    //  Sets a random background color from colors array for each random quote.
+    document.getElementById('color').style.backgroundColor = `${randomColor(colors)}`;
+}
+
+// Returns a random color from the colors array.
+function randomColor (colorArray) {
+    let indexColor = Math.floor( Math.random() * colorArray.length);
+    return colorArray[indexColor];
 }
 
 
